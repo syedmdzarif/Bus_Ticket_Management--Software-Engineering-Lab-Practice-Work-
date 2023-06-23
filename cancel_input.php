d7b3927cdffbad38e8173cba6c51e19db2d42d5b
Zarif011202009 <szarif202009@bscse.uiu.ac.bd>
2023-06-24 00:40:29 +0600

    Commit for frontend for delete

---
+++ cancel_input.php
@@ -0,0 +1,30 @@
+<?php
+	require_once('dbconfig.php');
+	$conn = mysqli_connect( HOST, USER, PASS, DB )
+		or die("Unable to establish connection");
+
+
+	$results = mysqli_query( $conn, "SELECT * FROM ticket_booking" )
+		or die("Can not execute query");
+
+	echo "<table border> \n";
+	echo "<th>ID</th> <th>Name</th> <th>Application Date</th> <th>From</th> <th>To</th> <th>Status</th> \n";
+
+	while( $rows = mysqli_fetch_array( $results ) ) {
+		extract( $rows );
+		echo "<tr>";
+		echo "<td> $ID </td>";
+		echo "<td> $NAME </td>";
+        echo "<td> $APPT_DATE </td>";
+		echo "<td> $FROM_LOCATION </td>";
+        echo "<td> $TO_LOCATION </td>";
+        echo "<td> $STATUS </td>";
+        echo "<td><button> <a href = 'cancel.php?id=$ID'> Cancel </a> </button></td>";
+		echo "</tr> \n";
+	}
+
+	echo "</table> \n";
+
+
+
+?>
