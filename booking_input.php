fe749c256ec8a53b81e684057be53351297a1a86
Zarif011202009 <szarif202009@bscse.uiu.ac.bd>
2023-06-23 22:49:26 +0600

    Commit for form creation

---
+++ booking_input.php
@@ -0,0 +1,16 @@
+<h1>Create Record</h1>
+
+
+
+<form method="" action="">
+
+	Name: <input type="text" name="name"> 
+    <br>
+    Date: <input type="text" name="date"> 
+    <br>
+    From: <input type="text" name="from"> 
+    <br>
+    To: <input type="text" name="to"> 
+    <br>
+
+</form>
